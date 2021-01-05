c1bd2f11baab3efa61a5235d0f1564020b4f09b1
HHHUUUGGGOOO <b07901103@ntu.edu.tw>
2021-01-05 16:24:08 +0800

    20210105

--- test.py
+++ test.py
@@ -1,62 +1,11 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute, Aer
-from matplotlib import pyplot as plt
-from qiskit.visualization import plot_histogram
-
 import numpy as np
-
-sim_qasm = Aer.get_backend('qasm_simulator')
-
-U1_q = QuantumRegister(2)
-U1_c = QuantumCircuit(U1_q, name='U1')
-U1 = U1_c.to_gate()
-U1_dag = U1.inverse()
-
-def construct_encode(U):
-    return U.control(1)
-
-def construct_decode(U):
-    circ = QuantumCircuit(3, name=U.name + '_decode')
-    circ.x(0)
-    circ.append(U.control(1), [0, 1, 2])
-    return circ.to_gate()
-
-def construct_alice(U):
-    circ = QuantumCircuit(3, name='Alice_' + U.name)
-    E = construct_encode(U)
-    circ.append(E, [0, 1, 2])
-    return circ.to_gate()
-
-def construct_bob(U):
-    circ = QuantumCircuit(3, name='Bob_' + U.name)
-    D = construct_decode(U)
-    circ.append(D, [0, 1, 2])
-    return circ.to_gate()
-
-ka = QuantumRegister(1, name='ka')
-kb = QuantumRegister(1, name='kb')
-m = QuantumRegister(2, name='m')
-c = ClassicalRegister(2)
-qc = QuantumCircuit(ka, kb, m, c)
-
-# Build EPR pair
-qc.h(ka)
-qc.cnot(ka, kb)
-qc.x(kb)
-qc.z(kb)
-qc.barrier(ka,kb)
-
-alice = construct_alice(U1)
-bob = construct_bob(U1)
-
-qc.append(alice, [ka, m[0], m[1]])
-qc.append(bob, [kb, m[0], m[1]])
-
-qc.measure(m, c)
-qc.draw(output='mpl')
-
-job = execute(qc, backend=sim_qasm, shots=1e6)
-result = job.result()
-counts = result.get_counts()
-plot_histogram(counts)
-
-plt.show()
+# M = np.identity(2)
+# N = np.zeros([2, 2])
+# O = np.identity(2)
+# P = np.zeros([2, 2])
+# A = np.hstack((M, N))
+# B = np.hstack((P, O))
+# C = np.vstack((A, B))
+# print(C)
+a = -3
+print(abs(a))
